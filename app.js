3240148f3e6209c7394637189be1b619a1433abd
SaveryIV <soriaagustin8@gmail.com>
2023-07-12 06:00:08 -0300

    Fixing eslint error

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ $addButton.addEventListener('click', () => {
 
 document.addEventListener('DOMContentLoaded', () => {
   const booksSaved = JSON.parse(localStorage.getItem('books'));
-  books.push(booksSaved);
+  myBooks.books.push(booksSaved);
 
   if (booksSaved) {
     myBooks.books = booksSaved;
